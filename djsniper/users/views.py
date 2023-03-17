a42c3c351ad21e5ebb0f61a173bbd11119b54e99
nayru <nayru@gigamonos.com>
2023-03-17 16:33:24 +0000

    adding user detail view separated roles

--- djsniper/users/views.py
+++ djsniper/users/views.py
@@ -57,15 +57,17 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     slug_field = "username"
     slug_url_kwarg = "username"
     #template_name = "users/dashboard.html"
-    if self.request.user is not None:
-        if self.request.user.role == "Desarrollador":
-            template_name = "dashboard/developer.html"
-        elif self.request.user.role == "Persona Natural":
-            template_name == "users/dashboard.html"
-        elif self.request.user.role == "Empresa":
-            template_name == "dashboard/enterprise.html"
-        else:
-            print(self.request.user.role)
+    def get_redirect_url(self):
+        if self.request.user is not None:
+            #login(request, user)
+            if self.request.user.role == "Desarrollador":
+                return reverse('developer_home')
+            elif self.request.user.role == "Persona Natural":
+                return reverse('investor_home')
+            elif self.request.user.role == "Empresa":
+                return reverse('enterprise_home')
+            else:
+                print(self.request.user.role)
 
 
 user_detail_view = UserDetailView.as_view()
