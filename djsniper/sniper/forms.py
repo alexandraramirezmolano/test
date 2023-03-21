da56ff55f4d99efc3a2558d3f452e1d066162a8e
nayru <nayru@gigamonos.com>
2023-03-21 15:54:10 +0000

    form style template

--- djsniper/sniper/forms.py
+++ djsniper/sniper/forms.py
@@ -29,16 +29,16 @@ class ProjectForm(forms.ModelForm):
         super(ProjectForm, self).__init__(*args, **kwargs)
         
         # update widget attributes
-        self.fields['name'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Name'})
+        self.fields['name'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Nombre'})
         self.fields['contract_address'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Contract Address'})
-        self.fields['image'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Image'})
-        self.fields['number_of_nfts'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Number of NFTs'})
-        self.fields['supply'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Supply'})
-        self.fields['price'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Price'})
+        self.fields['image'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Imagen'})
+        self.fields['number_of_nfts'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Cantidad de bonos'})
+        self.fields['supply'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Bonos disponibles'})
+        self.fields['price'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Precio unitario'})
         self.fields['chain'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Chain'})
         self.fields['contract_abi'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Contract ABI', 'rows': 5})
-        self.fields['category'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Category'})
-        self.fields['description'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Description', 'rows': 5})
+        self.fields['category'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Categor+ia'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Descripción', 'rows': 5})
         
         # divide the fields into two columns
         for field_name in self.fields:
