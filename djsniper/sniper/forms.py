f9e19aac91e096bf9a0d13cb289b1de317c7ada1
nayru <nayru@gigamonos.com>
2023-03-21 14:09:38 +0000

    adding widgets

--- djsniper/sniper/forms.py
+++ djsniper/sniper/forms.py
@@ -6,6 +6,18 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = NFTProject
         fields = '__all__'
+        widgets = {
+            'contract_address': forms.TextInput(attrs={'class': 'form-control'}),
+            'contract_abi': forms.Textarea(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'number_of_nfts': forms.NumberInput(attrs={'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'supply': forms.NumberInput(attrs={'class': 'form-control'}),
+            'price': forms.TextInput(attrs={'class': 'form-control'}),
+            'chain': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+        }
 
 class CategoryForm(forms.ModelForm):
     class Meta:
