f30bb0fa7892cba15d801ec93414213488fc9cb5
nayru <nayru@gigamonos.com>
2023-03-21 16:36:13 +0000

    form style template

--- djsniper/sniper/models.py
+++ djsniper/sniper/models.py
@@ -28,7 +28,7 @@ class NFTProject(models.Model):
     description = models.CharField(max_length=500, null=True)
     coin = CharField(max_length=20, default="USD", blank=False, editable=False)
     private = models.BooleanField(default=True)
-    enterprise_id = models.ForeignKey(config.settings.base.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    enterprise_id = models.ForeignKey(config.settings.base.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, default=None)
     developers = models.ManyToManyField(config.settings.base.AUTH_USER_MODEL, related_name='nft_projects', null=True, blank=True)
 
     class Meta:
