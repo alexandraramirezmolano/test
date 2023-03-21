2465cb4bea8197a9485879b956c753ea6a000ba8
nayru <nayru@gigamonos.com>
2023-03-21 10:28:43 +0000

    get model fields another way

--- djsniper/sniper/admin.py
+++ djsniper/sniper/admin.py
@@ -28,10 +28,10 @@ class NFTAttributeAdmin(admin.ModelAdmin):
 
 class NFTProjectAdmin(admin.ModelAdmin):
     model = NFTProject
-    list_display = [field.name for field in NFTProject._meta.get_fields() if not isinstance(field, model.ManyToOneRel)]
+    list_display = [field.name for field in NFTProject._meta.get_fields() if not isinstance(field, NFTProject.ManyToOneRel)]
     list_filter = list_display
     search_fields = list_display
-    search_help_text = ["Buscar por {}".format(field.verbose_name) for field in NFTProject._meta.get_fields() if not isinstance(field, model.ManyToOneRel)]
+    search_help_text = ["Buscar por {}".format(field.verbose_name) for field in NFTProject._meta.get_fields() if not isinstance(field, NFTProject.ManyToOneRel)]
 
 
 
