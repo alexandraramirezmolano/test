b52f5c9da24d63f2dedcb301b10e313df1a6c457
nayru <nayru@gigamonos.com>
2023-03-21 15:01:46 +0000

    form style template

--- djsniper/sniper/forms.py
+++ djsniper/sniper/forms.py
@@ -36,6 +36,29 @@ class ProjectForm(forms.ModelForm):
             'price': {'required': 'Por favor ingrese un precio para el proyecto'},
             'description': {'required': 'Por favor ingrese una descripción para el proyecto'},
         }
+        def __init__(self, *args, **kwargs):
+            super(ProjectForm, self).__init__(*args, **kwargs)
+            self.fields['contract_address'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Contract Address'})
+            self.fields['contract_abi'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Contract ABI', 'rows': 5})
+            self.fields['name'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Name'})
+            self.fields['number_of_nfts'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Number of NFTs'})
+            self.fields['image'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Image'})
+            self.fields['category'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Category'})
+            self.fields['supply'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Supply'})
+            self.fields['price'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Price'})
+            self.fields['chain'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Chain'})
+            self.fields['description'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Description', 'rows': 5})
+            # divide the fields into two columns
+            self.fields['name'].widget.attrs['class'] = 'form-control col-md-6'
+            self.fields['contract_address'].widget.attrs['class'] = 'form-control col-md-6'
+            self.fields['contract_abi'].widget.attrs['class'] = 'form-control col-md-6'
+            self.fields['number_of_nfts'].widget.attrs['class'] = 'form-control col-md-6'
+            self.fields['image'].widget.attrs['class'] = 'form-control col-md-6'
+            self.fields['category'].widget.attrs['class'] = 'form-control col-md-6'
+            self.fields['supply'].widget.attrs['class'] = 'form-control col-md-6'
+            self.fields['price'].widget.attrs['class'] = 'form-control col-md-6'
+            self.fields['chain'].widget.attrs['class'] = 'form-control col-md-6'
+            self.fields['description'].widget.attrs['class'] = 'form-control col-md-6'
 
 class CategoryForm(forms.ModelForm):
     class Meta:
