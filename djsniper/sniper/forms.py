76a66eb93b017e49c6f853bba6fd17e09c39f859
nayru <nayru@gigamonos.com>
2023-03-21 15:05:35 +0000

    form style template

--- djsniper/sniper/forms.py
+++ djsniper/sniper/forms.py
@@ -6,18 +6,6 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = NFTProject
         fields = '__all__'
-        widgets = {
-            'contract_address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Contract Address'}),
-            'contract_abi': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Contract ABI'}),
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}),
-            'number_of_nfts': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of NFTs'}),
-            'image': forms.ClearableFileInput(attrs={'class': 'form-control', 'placeholder': 'Image'}),
-            'category': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Category'}),
-            'supply': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Supply'}),
-            'price': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Price'}),
-            'chain': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Chain'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Description'}),
-        }
         labels = {
             'contract_address': 'Contrato',
             'contract_abi': 'ABI del Contrato',
