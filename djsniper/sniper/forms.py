ab873fd9c5e62f5c7084db53c6a1f14ac590655b
nayru <nayru@gigamonos.com>
2023-03-21 14:46:54 +0000

    form style

--- djsniper/sniper/forms.py
+++ djsniper/sniper/forms.py
@@ -7,16 +7,34 @@ class ProjectForm(forms.ModelForm):
         model = NFTProject
         fields = '__all__'
         widgets = {
-            'contract_address': forms.TextInput(attrs={'class': 'form-control'}),
-            'contract_abi': forms.Textarea(attrs={'class': 'form-control'}),
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'number_of_nfts': forms.NumberInput(attrs={'class': 'form-control'}),
-            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
-            'supply': forms.NumberInput(attrs={'class': 'form-control'}),
-            'price': forms.TextInput(attrs={'class': 'form-control'}),
-            'chain': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'contract_address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Contract Address'}),
+            'contract_abi': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Contract ABI'}),
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}),
+            'number_of_nfts': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Number of NFTs'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control', 'placeholder': 'Image'}),
+            'category': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Category'}),
+            'supply': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Supply'}),
+            'price': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Price'}),
+            'chain': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Chain'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Description'}),
+        }
+        labels = {
+            'contract_address': 'Contrato',
+            'contract_abi': 'ABI del Contrato',
+            'name': 'Nombre',
+            'number_of_nfts': 'Cantidad de bonos',
+            'image': 'Imagen',
+            'category': 'Categoría',
+            'supply': 'Disponibles',
+            'price': 'Precio unitario',
+            'chain': 'Cadena',
+            'description': 'Descripción',
+        }
+        error_messages = {
+            'name': {'required': 'Por favor, introduzca un nombre para el proyecto.'},
+            'number_of_nfts': {'required': 'Ingrese el número de bonos para el proyecto'},
+            'price': {'required': 'Por favor ingrese un precio para el proyecto'},
+            'description': {'required': 'Por favor ingrese una descripción para el proyecto'},
         }
 
 class CategoryForm(forms.ModelForm):
