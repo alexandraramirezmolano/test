cefd2bd60b0db809aff0970532b0d560330f4ea4
nayru <nayru@gigamonos.com>
2023-03-22 21:31:14 +0000

    updating developer models

--- djsniper/developers/admin.py
+++ djsniper/developers/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Developer
+from djsniper.users.admin import UserAdmin
 # Register your models here.
 class DeveloperAdmin(UserAdmin):
     model = Developer
