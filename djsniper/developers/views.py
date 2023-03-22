9b1b34323b6fb9bb96772164074f2d82211a8ee1
nayru <nayru@gigamonos.com>
2023-03-22 21:15:31 +0000

    adding developers views

--- djsniper/developers/views.py
+++ djsniper/developers/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
+from django.urls import reverse_lazy
+from .models import Developer
 
-# Create your views here.
+class DeveloperListView(ListView):
+    model = Developer
+    template_name = 'dashboard/enterprise/developer_list.html'
+
+class DeveloperCreateView(CreateView):
+    model = Developer
+    fields = '__all__'
+    template_name = 'dashboard/enterprise/developer_form.html'
+    success_url = reverse_lazy('developers:list')
+
+class DeveloperUpdateView(UpdateView):
+    model = Developer
+    fields = '__all__'
+    template_name = 'dashboard/enterprise/developer_form.html'
+    success_url = reverse_lazy('developers:list')
+
+class DeveloperDetailView(DetailView):
+    model = Developer
+    template_name = 'dashboard/enterprise/developer_detail.html'
