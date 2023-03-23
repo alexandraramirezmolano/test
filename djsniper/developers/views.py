754da12104df608beb5120a034d8a37bc82ed857
nayru <nayru@gigamonos.com>
2023-03-23 20:55:59 +0000

    migrate

--- djsniper/developers/views.py
+++ djsniper/developers/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .forms import DeveloperProjectForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class DeveloperListView(ListView):
