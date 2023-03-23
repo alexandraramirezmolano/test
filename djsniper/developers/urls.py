be31a9a43e1be7ba234f460f6052ff0760de0139
nayru <nayru@gigamonos.com>
2023-03-23 20:06:30 +0000

    adding developers dashboard

--- djsniper/developers/urls.py
+++ djsniper/developers/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('create/', DeveloperCreateView.as_view(), name='developer-create'),
     path('<int:pk>/update/', DeveloperUpdateView.as_view(), name='developer-update'),
     path('<int:pk>/', DeveloperDetailView.as_view(), name='developer-detail'),
+
+    path('projects-list/', NFTProjectListView.as_view(), name='project_list'),
+    path('project/<uuid:pk>/', NFTProjectDetailView.as_view(), name='project_detail')
 ]
--- djsniper/developers/views.py
+++ djsniper/developers/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from django.urls import reverse_lazy
 from .models import Developer
+from djsniper.sniper.models import NFTProject
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 class DeveloperListView(ListView):
     model = Developer
@@ -21,3 +26,15 @@ class DeveloperUpdateView(UpdateView):
 class DeveloperDetailView(DetailView):
     model = Developer
     template_name = 'dashboard/enterprise/developer_detail.html'
+
+
+class NFTProjectListView(ListView):
+    model = NFTProject
+    template_name = 'project_list.html'
+    context_object_name = 'projects'
+    ordering = ['-id']
+
+class NFTProjectDetailView(DetailView):
+    model = NFTProject
+    template_name = 'project_detail.html'
+    context_object_name = 'project'
