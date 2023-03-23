8a63b752df2e24c651ae19028959d093d3981bf6
nayru <nayru@gigamonos.com>
2023-03-23 14:26:09 +0000

    adding app_name urls

--- djsniper/enterprise/admin.py
+++ djsniper/enterprise/admin.py
@@ -5,7 +5,7 @@ from .models import Enterprise
 
 class EnterpriseAdmin(UserAdmin):
     model = Enterprise
-    list_display = ["username", "name", "email",  "image", "nit", "allowed", "phone", "contact", "role", "display_projects"]
+    list_display = ["username", "name", "email",  "image", "nit", "allowed", "phone", "contact", "role"]
     list_editable = ["image", "allowed", "phone", "nit", "contact", "role"]
     list_filter = [ "allowed", "role"]
     search_fields = ["username", "name", "email"]
