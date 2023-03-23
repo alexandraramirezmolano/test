bca5b4c12dc09cdeee39e7259534ddd888c78e0d
nayru <nayru@gigamonos.com>
2023-03-23 15:48:34 +0000

    enterprise projects view

--- djsniper/enterprise/views.py
+++ djsniper/enterprise/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render
 from djsniper.sniper.models import NFTProject
-# Create your views here.
 from django.views.generic.list import ListView
 from django.views import generic
 
 
-class EnterpriseProjectsView(generic.ListView):
+class EnterpriseProjectsView(ListView):
     template_name = "dashboard/enterprise/projects_list.html"
     model = NFTProject
 
     def get_queryset(self):
         return NFTProject.objects.all()
 
-
-    def filter_by_enteprise(self, enterprise=self.request.user.id):
+    def filter_by_enteprise(self, enterprise=None):
+        enterprise = enterprise or self.request.user.id
         filtered_projects = []
         for project in self.projects:
             if project.enterprise == enterprise:
                 filtered_projects.append(project)
-                grouped_objects = filtered_projects
-        return print(grouped_objects)
+        return filtered_projects
