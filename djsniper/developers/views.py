36d3ee3c56fd46927fdc15554a583e02cfdb23f7
nayru <nayru@gigamonos.com>
2023-03-23 20:31:03 +0000

    migrate

--- djsniper/developers/views.py
+++ djsniper/developers/views.py
@@ -27,13 +27,17 @@ class DeveloperDetailView(DetailView):
     model = Developer
     template_name = 'dashboard/enterprise/developer_detail.html'
 
-
 class NFTProjectListView(ListView):
     model = NFTProject
     template_name = 'dashboard/developer/project_list.html'
     context_object_name = 'projects'
     ordering = ['-id']
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(developer=self.request.user.id)
+
+        
 class NFTProjectDetailView(DetailView):
     model = NFTProject
     template_name = 'dashboard/developer/project_detail.html'
