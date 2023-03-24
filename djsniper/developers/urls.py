8e93036f8f81400ddecbc0903b2a530588b1288a
nayru <nayru@gigamonos.com>
2023-03-24 15:15:45 +0000

    add create project developer

--- djsniper/developers/urls.py
+++ djsniper/developers/urls.py
@@ -4,7 +4,7 @@ from .views import DeveloperListView, DeveloperCreateView, DeveloperUpdateView, 
 app_name = 'developers'
 
 urlpatterns = [
-    path('', DeveloperListView.as_view(), name='developers-list'),
+    path('list/', DeveloperListView.as_view(), name='developers-list'),
     path('create/', DeveloperCreateView.as_view(), name='developer-create'),
     path('<int:pk>/update/', DeveloperUpdateView.as_view(), name='developer-update'),
     path('<int:pk>/', DeveloperDetailView.as_view(), name='developer-detail'),
