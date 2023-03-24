59a55bcacd6dd97784f68995bfd3cddb16fdba5b
nayru <nayru@gigamonos.com>
2023-03-24 22:11:17 +0000

    style

--- djsniper/users/views.py
+++ djsniper/users/views.py
@@ -39,7 +39,7 @@ def login_view(request):
         username = request.POST['username']
         password = request.POST['password']
         #user = authenticate(request, username=username, password=password)
-        user = authenticate(request, username=username, password=password, backend='django.contrib.auth.backends.ModelBackend')
+        user = authenticate(request, username=username, password=password, backend='django.contrib.auth.backends.ModelBackend', **kwargs)
 
 
         if user is not None:
