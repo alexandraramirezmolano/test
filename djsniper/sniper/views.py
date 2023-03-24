ba04c0f182d30705da2e9eb2fd8e1a6169cbdcb7
nayru <nayru@gigamonos.com>
2023-03-24 18:55:00 +0000

    add create project developer

--- djsniper/sniper/views.py
+++ djsniper/sniper/views.py
@@ -189,16 +189,18 @@ class OrderCreateView(generic.CreateView):
         return Order.objects.all()
 
 class ProjectCreateView(generic.CreateView):
-    template_name = "sniper/project_create.html"
-    form_class = EnterpriseProjectForm
 
     def form_valid(self, form):
         user = self.request.user
         
         instance = form.save()
         if user.role == "Desarrollador":
+            template_name = "dashboard/developer/project_create.html"
+            form_class = DeveloperProjectForm
             return redirect("developers:project-list")
         elif user.role == "Empresa":
+            template_name = "sniper/project_create.html"
+            form_class = EnterpriseProjectForm
             return redirect("enterprise:enterprise-projects", username=self.request.user.username)
 
     def get_queryset(self):
