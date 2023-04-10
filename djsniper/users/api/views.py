d6e7e45ba022f461cb179e65571389ebba6bfa88
nayru <nayru@gigamonos.com>
2023-04-10 18:54:17 +0000

    adding viewsets

--- djsniper/users/api/views.py
+++ djsniper/users/api/views.py
@@ -1,11 +1,32 @@
 from django.contrib.auth import get_user_model
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from .serializers import UserSerializer
+
+from djsniper.users.models import (
+    Role,
+    CustomGroup,
+    CustomPermission,
+    PaymentMethod,
+    CoalBonus,
+    Project,
+    PurchaseOrder,
+)
+from .serializers import (
+    RoleSerializer,
+    CustomGroupSerializer,
+    CustomPermissionSerializer,
+    UserSerializer,
+    PaymentMethodSerializer,
+    CoalBonusSerializer,
+    ProjectSerializer,
+    PurchaseOrderSerializer,
+)
+
 
 User = get_user_model()
 
@@ -23,3 +44,42 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
     def me(self, request):
         serializer = UserSerializer(request.user, context={"request": request})
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+
+class CustomGroupViewSet(viewsets.ModelViewSet):
+    queryset = CustomGroup.objects.all()
+    serializer_class = CustomGroupSerializer
+
+
+class CustomPermissionViewSet(viewsets.ModelViewSet):
+    queryset = CustomPermission.objects.all()
+    serializer_class = CustomPermissionSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PaymentMethodViewSet(viewsets.ModelViewSet):
+    queryset = PaymentMethod.objects.all()
+    serializer_class = PaymentMethodSerializer
+
+
+class CoalBonusViewSet(viewsets.ModelViewSet):
+    queryset = CoalBonus.objects.all()
+    serializer_class = CoalBonusSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class PurchaseOrderViewSet(viewsets.ModelViewSet):
+    queryset = PurchaseOrder.objects.all()
+    serializer_class = PurchaseOrderSerializer
