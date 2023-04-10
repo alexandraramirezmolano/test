8997cbade23145dec852e42ea03c41ecd24fcd30
nayru <nayru@gigamonos.com>
2023-04-10 14:28:43 +0000

    adding migrations

--- djsniper/users/models.py
+++ djsniper/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, PermissionsMixin, Permission
 import uuid
+
 
 class Role(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
@@ -12,34 +13,54 @@ class Role(models.Model):
     delete = models.BooleanField(default=False)
     list = models.BooleanField(default=False)
 
-    
-        
+
+class CustomGroup(Group):
+    class Meta:
+        proxy = True
 
 
-class User(AbstractUser):
+class CustomPermission(Permission):
+    class Meta:
+        proxy = True
+
+
+class User(AbstractUser, PermissionsMixin):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=255)
     image = models.ImageField()
     verified = models.BooleanField(default=False)
     nit = models.CharField(max_length=255)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True, blank=True)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True, blank=True, related_name='users')
+    groups = models.ManyToManyField(Group, blank=True, related_name='user_set', through='CustomGroup')
+    user_permissions = models.ManyToManyField(Permission, blank=True, related_name='user_set',
+                                              through='CustomPermission')
+    groups = models.ManyToManyField(
+        CustomGroup,
+        verbose_name=('groups'),
+        blank=True,
+        related_name='custom_user_set',
+        related_query_name='custom_user'
+    )
 
-    
+    user_permissions = models.ManyToManyField(
+        CustomPermission,
+        verbose_name=('user permissions'),
+        blank=True,
+        related_name='custom_user_set',
+        related_query_name='custom_user'
+    )
+
 
 class PaymentMethod(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.TextField()
     api_key = models.TextField()
 
-    
-
 
 class CoalBonus(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     contract = models.TextField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='coal_bonuses')
 
 
 class Project(models.Model):
@@ -59,8 +80,6 @@ class Project(models.Model):
     verified = models.BooleanField(default=False)
     ton_of_carbon_equivalent = models.IntegerField()
 
-    
-
     def __str__(self):
         return self.name
 
@@ -74,7 +93,5 @@ class PurchaseOrder(models.Model):
     payment_method = models.ForeignKey(PaymentMethod, on_delete=models.CASCADE)
     verified = models.BooleanField(default=False)
 
-    
-        
     def __str__(self):
         return f"{self.customer.username}'s order for {self.project.name}"
