14ce99030a3156f95b94aa4204f8c4c34a8dc473
nayru <nayru@gigamonos.com>
2023-04-10 17:13:53 +0000

    adding serilaizers

--- djsniper/users/api/serializers.py
+++ djsniper/users/api/serializers.py
@@ -1,15 +1,75 @@
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
+#from django.contrib.auth import get_user_model
+#from rest_framework import serializers
 
-User = get_user_model()
+#User = get_user_model()
+
+
+#class UserSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = User
+#        fields = ["username", "name", "url"]
+
+#        extra_kwargs = {
+#            "url": {"view_name": "api:user-detail", "lookup_field": "username"}
+#        }
+
+from rest_framework import serializers
+from .models import User, Role, CustomGroup, CustomPermission, PaymentMethod, CoalBonus, Project, PurchaseOrder
+
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = '__all__'
+
+
+class CustomGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomGroup
+        fields = '__all__'
+
+
+class CustomPermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomPermission
+        fields = '__all__'
 
 
 class UserSerializer(serializers.ModelSerializer):
+    groups = CustomGroupSerializer(many=True, read_only=True)
+    user_permissions = CustomPermissionSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ["username", "name", "url"]
+        fields = ('user_id', 'username', 'email', 'name', 'image', 'verified', 'nit', 'role', 'groups', 'user_permissions', 'is_superuser')
 
-        extra_kwargs = {
-            "url": {"view_name": "api:user-detail", "lookup_field": "username"}
-        }
 
+class PaymentMethodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaymentMethod
+        fields = '__all__'
+
+
+class CoalBonusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CoalBonus
+        fields = '__all__'
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    enterprise = UserSerializer()
+    developer = UserSerializer()
+    investor = UserSerializer()
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class PurchaseOrderSerializer(serializers.ModelSerializer):
+    customer = UserSerializer()
+    project = ProjectSerializer()
+
+    class Meta:
+        model = PurchaseOrder
+        fields = '__all__'
